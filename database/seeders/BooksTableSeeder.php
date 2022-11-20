8d265474e129612e0cdf0c7b62dcaaa6247dec2e
ysigawa <ys.igawa@gmail.com>
2022-11-20 14:44:26 +0900

    add all

---
+++ database/seeders/BooksTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // ←これを追加
+
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // テーブルのクリア
+        DB::table('books')->truncate();
+
+        // 初期データ用意（列名をキーとする連想配列）
+        $books = [
+              ['name' => 'PHP Book',
+               'price' => 2000,
+               'author' => 'PHPER'],
+              ['name' => 'Laravel Book',
+               'price' => 3000,
+               'author' => null],
+              ['name' => 'Ruby Book',
+               'price' => 2500,
+               'author' => 'Rubyist']
+             ];
+
+        // 登録
+        foreach($books as $book) {
+            \App\Models\Book::create($book);
+        }
+    }
+}
